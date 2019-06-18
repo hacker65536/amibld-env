05cd530526fd913efbf1e19c5bd748fac71afb1d
hacker65536 <s.hacker65536@gmail.com>
2019-06-18 01:33:27 +0000

    Update

--- codebuild/output.tf
+++ codebuild/output.tf
@@ -1,3 +1,7 @@
-output "badges"{
-value =aws_codebuild_project.codebuild.*.badge_url
+output "badges" {
+  value = aws_codebuild_project.codebuild.*.badge_url
+}
+
+output "codebuild_subnet" {
+  value = data.aws_subnet_ids.pri_nat.ids
 }
